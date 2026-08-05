e326dfc13bef3917b0111b2dc816d8f03b8c83bf
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-08-05 14:50:52 +0100

    create Task model class

---
+++ backend/src/main/java/com/mansouryassine/task_manager/enums/Status.java
@@ -0,0 +1,7 @@
+package com.mansouryassine.task_manager.enums;
+
+public enum Status {
+    ToDo,
+    In_Progress,
+    Done
+}
--- backend/src/main/java/com/mansouryassine/task_manager/model/Task.java
+++ backend/src/main/java/com/mansouryassine/task_manager/model/Task.java
@@ -1,5 +1,32 @@
 package com.mansouryassine.task_manager.model;
 
+import com.mansouryassine.task_manager.enums.Priority;
+import com.mansouryassine.task_manager.enums.Status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Priority priority;
+    private Status status;
+    private Date dueDate;
+    private String createdBy;
+    private String assignedTo;
+
 }
