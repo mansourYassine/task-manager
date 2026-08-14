52334a56b65804a0acde4247119970c26e869104
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-08-14 17:26:46 +0100

    increase the page name title in the header

--- frontend/src/pages/AllTasks.jsx
+++ frontend/src/pages/AllTasks.jsx
@@ -40,7 +40,7 @@ export default function AllTasks() {
     return (
         <>
             <header className=" flex justify-between py-4 px-4.5 sm:py-4.5 sm:px-5.5 lg:py-5 lg:px-7 border-b border-[#d4d4d8] ">
-                <h2 className=" font-bold text-2xl text-custom-dark ">All tasks</h2>
+                <h2 className=" font-bold text-3xl text-custom-dark ">All tasks</h2>
                 <div className=" flex items-center justify-between gap-2.5 ">
                     <div className=" hidden sm:flex items-center bg-cstmbg-secondary border border-[#d4d4d8] rounded-md py-2 ">
                         <FontAwesomeIcon className=" text-[#b6b6b6] px-2 text-sm " icon={faMagnifyingGlass} />
