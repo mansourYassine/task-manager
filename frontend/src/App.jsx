f1998def1e5407408f59f4710519d45846debcb5
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-09-04 19:38:31 +0100

    Create Not Found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import Layout from './components/Layout';
 import AllTasks, {loader as allTasksLoader} from './pages/AllTasks'
+import NotFound from './pages/NotFound';
 
 export default function App() {
     const router = createBrowserRouter([
@@ -9,7 +10,11 @@ export default function App() {
             Component: Layout,
             children: [
                 {index: true, Component: AllTasks, loader: allTasksLoader}
-            ]
+            ]            
+        },
+        {
+            path: '*',
+            Component: NotFound
         }
     ])
     
