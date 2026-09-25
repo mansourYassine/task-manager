9f1b5aa32c290cc62f970e84f5bbd4ff7feddb12
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-09-25 18:03:46 +0000

    delete the spring boot backend folder

--- backend/src/main/java/com/mansouryassine/task_manager/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mansouryassine.task_manager.repository;
-
-import com.mansouryassine.task_manager.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
